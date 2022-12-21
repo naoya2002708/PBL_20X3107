1484f6a367b2cf236cfb9a6c6c71f7476a6e493f
naoya2002708 <naoya.takayama.3i@stu.hosei.ac.jp>
2022-12-21 16:33:33 +0900

    完成

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import re
-from flask import Flask, render_template, request,redirect,session
+from flask import Flask, render_template, request,redirect,session,make_response
 from werkzeug.security import generate_password_hash as gph
 from werkzeug.security import check_password_hash as cph
 import MySQLdb
 import html
+import datetime
+from dicttoxml import dicttoxml
+
 from datetime import timedelta
 import secrets
 
@@ -106,13 +109,247 @@ def login():
 @app.route("/home")
 def home():
     if "name" in session:
+        name=session["name"]
+        con=connect()
+        cur=con.cursor()
+        cur.execute("""
+                    SELECT SUM(steps) FROM record WHERE name=%(name)s
+                    """,{"name":name})
+        data=[]
+        for row in cur:
+            data.append(row)
         
-        return render_template("success.html",
-                               name=html.escape(session["name"]),
-                               email=html.escape(session["email"]))                      
+        print(data[0])
+        data = [str(i) for i in data]
+        dt_str = ",".join(data)
+        total = re.sub("\\D", "",dt_str )
+        print("total=",total)
+        if len(total)==0:
+            
+            session["total"]="0"
+        else:
+            print(total)
+            session["total"]=total
+
+        #今日の歩数追加分   
+        today=datetime.date.today()
+        print(today)
+        con=connect()
+        cur=con.cursor()
+        cur.execute("""
+                    SELECT SUM(steps) FROM record WHERE name=%(name)s AND record_date=%(record_date)s
+                    """,{"name":name,"record_date":today})
+        data=[]
+        for row in cur:
+            data.append(row)
+        
+        
+        data = [str(i) for i in data]
+        dt_str = ",".join(data)
+        today_record = re.sub("\\D", "",dt_str )
+        
+        if len(today_record)==0:
+            print("today=",len(today_record))
+            session["today_record"]=0
+        else:
+            print(today_record)
+            session["today_record"]=today_record
+            
+        
+        #カロリー消費量計算
+        
+        if session["today_record"]==0:
+            session["today_calorie"]=0
+        else:
+            height=session["height"]
+            weight=session["weight"]
+            kyou=float(today_record)
+            hohaba=height*0.4
+            hokoukyori=hohaba*kyou/100
+            print(hokoukyori)
+            hokouzikann=(hokoukyori/67)
+            print(hokouzikann)
+            Ex=hokouzikann/60*3.0
+            calorie=1.05*Ex*weight
+            session["today_calorie"]=round(calorie,2)
+            
+
+
+        con.commit()
+        con.close()
+        return render_template("mypage.html",
+                                total=session["total"],
+                                name=html.escape(session["name"]),
+                                today_calorie=session["today_calorie"],
+                                today_record=session["today_record"])
+                     
     else:
         return redirect("login")
 
+    
+
+
+
+
+@app.route("/record",methods=["GET","POST"])
+def record():
+    if "name" in session:
+        if request.method == "GET":
+            return render_template("record.html")
+        elif request.method == "POST":
+            
+            name =session["name"]
+            email = session["email"]
+            age = session["age"]
+            sex = session["sex"]
+            height = session["height"]
+            weight = session["weight"]
+            record_date = request.form["record_date"]
+            steps = request.form["steps"]
+            memo = request.form["memo"]
+            if len(steps)==0:
+                calorie=0
+            else:
+                height=session["height"]
+                weight=session["weight"]
+                kyou=float(steps)
+                hohaba=height*0.4
+                hokoukyori=hohaba*kyou/100
+                print(hokoukyori)
+                hokouzikann=(hokoukyori/67)
+                print(hokouzikann)
+                Ex=hokouzikann/60*3.0
+                calorie=1.05*Ex*weight
+                calorie=round(calorie,2)
+            
+            #総歩数
+            name=session["name"]
+            con=connect()
+            cur=con.cursor()
+            cur.execute("""
+                        SELECT SUM(steps) FROM record WHERE name=%(name)s
+                        """,{"name":name})
+            data=[]
+            for row in cur:
+                data.append(row)
+            
+            print(data[0])
+            data = [str(i) for i in data]
+            dt_str = ",".join(data)
+            total = re.sub("\\D", "",dt_str )
+            if len(total)==0:
+                total=int(steps)
+
+            else:
+                total= int(total)
+                total=total+int(steps)
+            
         
+
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+                        INSERT INTO record
+                        (name,email,height,weight,sex,age,record_date,steps,memo,calorie,total)
+                        VALUES (%(name)s,%(email)s,%(height)s,%(weight)s,%(sex)s,%(age)s,%(record_date)s,%(steps)s,%(memo)s,%(calorie)s,%(total)s)
+                        """,{"name":name,"email":email,"height":height,"weight":weight,"sex":sex,"age":age,"record_date":record_date,"steps":steps,"memo":memo,"calorie":calorie,"total":total})
+            con.commit()
+            con.close()
+            
+            return redirect("/home")
+    else:
+        
+        return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/search",methods=["GET","POST"])
+def search():
+    if request.method == "GET":
+        return render_template("search.html")
+    elif request.method == "POST":
+        date = html.escape(request.form["date"])
+        ranking = html.escape(request.form["ranking"])
+        form = html.escape(request.form["format"])
+
+        if ranking=="steps":
+            ran="歩数"
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+            SELECT name,steps,memo
+            FROM record
+            WHERE record_date=%(date)s 
+            ORDER BY steps DESC
+            """,{"date":date})
+
+            
+        elif ranking=="calorie":
+            ran="カロリー"
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+            SELECT name,calorie,memo
+            FROM record
+            WHERE record_date=%(date)s
+            ORDER BY calorie DESC
+            """,{"date":date})
+
+
+
+        elif ranking=="sum_steps":
+            ran="今までの総歩数"
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+            SELECT name,total,memo
+            FROM record
+            WHERE id IN(SELECT MAX(id) FROM record GROUP BY name)
+            ORDER BY total DESC
+            """,{"date":date})
+
+            
+
+        if form == "JSON":
+            res = {}
+            tmpa = []
+            rank = 1
+            for row in cur:
+                dic = {}
+                dic["rank"] = rank
+                dic["name"] = row[0]
+                dic["count"] = row[1]
+                dic["memo"] = row[2]
+                tmpa.append(dic)
+                rank += 1
+            res["content"] = tmpa
+            con.commit()
+            con.close()
+            return render_template("result.html",res=res,ranking=ran)
+
+        elif form =="XML":
+            res = {}
+            tmpa = {}
+            dic ={}
+            rank = 1
+            for row in cur:
+                dic['rank'+str(rank)]=rank
+                dic['name'+str(rank)]= row[0]
+                dic['count'+str(rank)]= row[1]
+                dic['memo'+str(rank)]=row[2]
+                rank +=1
+            xml = dicttoxml(dic)
+            resp = make_response(xml)
+            resp.mimetype = "text/xml"
+            con.commit()
+            con.close()
+            return resp
+
+
 if __name__=="__main__":
     app.run(host="0.0.0.0")
